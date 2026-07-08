13de21ece941b44545393c54e1927f469c161a89
MohauzFN <mohauzfort@gmail.com>
2026-07-08 09:51:16 +0100

    Implement RSI function for trading signals
    Added RSI function to calculate Relative Strength Index and generate trading signals based on oversold and overbought conditions.

--- strategies.py
+++ strategies.py
@@ -17,3 +17,22 @@ def moving_average_crossover(df, short_window=50, long_window=200):
     df.loc[df['Short_MA'] <= df['Long_MA'], 'Signal'] = -1  # SELL signal
 
     return df
+
+def rsi(df, window = 14, oversold = 30, overbought = 70):
+    df = df.copy()
+
+    #RSI Calculation
+    delta = df['Close'].diff()
+    gain = delta.where(delta > 0, 0).rolling(window=window).mean()
+    loss = -delta.where(delta < 0, 0).rolling(window=window).mean()
+    rs = gain / loss
+    df['RSI'] = 100 - (100 / (1 + rs))
+
+    df['Signal'] = None
+    df.loc[df['RSI'] < oversold, 'Signal'] = 1
+    df.loc[df['RSI'] > overbought, 'Signal'] = -1
+    
+    # Holds the position until an opposite signal occurs
+    # (Replacing 0s with previous positions, ignoring the initial warm-up)
+    df['Signal'] = df['Signal'].ffill().fillna(0)
+    return df
